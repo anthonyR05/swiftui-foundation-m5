aafab2de06ef656745b8d1a6449362031f1f85af
anthonyR05 <anthrosa12@gmail.com>
2021-11-30 12:53:47 -0600

    - rename content view - organize files into folders - add content model - set content model as environment object

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by PM CSS3 - Anthony Rosales on 11/30/21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+}
