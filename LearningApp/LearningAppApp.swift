ad02da56b2bfe2c58483a7e235f42adbbd0d5f72
anthonyR05 <anthrosa12@gmail.com>
2021-11-30 12:19:55 -0600

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by PM CSS3 - Anthony Rosales on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
